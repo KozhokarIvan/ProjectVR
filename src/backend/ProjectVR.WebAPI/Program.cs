67c1ba9b9dda12a0675ba6aace1108c4dbd64b8f
KozhokarIvan <kozhokar.iv@yandex.ru>
2023-07-19 19:19:48 +0300

    added serilog and seq

--- src/backend/ProjectVR.WebAPI/Program.cs
+++ src/backend/ProjectVR.WebAPI/Program.cs
@@ -4,11 +4,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using ProjectVR.BusinessLogic.Services;
 using ProjectVR.DataAccess;
 using ProjectVR.DataAccess.Repositories;
 using ProjectVR.Domain.Interfaces.Repositories;
 using ProjectVR.Domain.Interfaces.Services;
+using Serilog;
 
 namespace ProjectVR.WebAPI
 {
@@ -18,45 +20,68 @@ namespace ProjectVR.WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddCors(options =>
-                options.AddPolicy("CorsPolicy", builder =>
-                {
-                    builder.WithOrigins("http://localhost:3000", "https://localhost:3000");
-                }));
-            builder.Services.AddControllers();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
-            string connectionString = builder.Configuration.GetConnectionString(nameof(ProjectVRDbContext));
-            builder.Services.AddDbContext<ProjectVRDbContext>(options =>
-                    options.UseNpgsql(connectionString));
-
-            builder.Services.AddScoped<IUsersService, UsersService>();
-            builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                builder.Services.AddLogging(configuration =>
+                {
+                    configuration.ClearProviders();
+                    configuration.AddConsole();
+                    configuration.AddSerilog(logger);
+                });
+
+                // Add services to the container.
+
+                builder.Services.AddCors(options =>
+                    options.AddPolicy("CorsPolicy", builder =>
+                    {
+                        builder.WithOrigins("http://localhost:3000", "https://localhost:3000");
+                    }));
+                builder.Services.AddControllers();
+
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddHttpLogging(options => { });
+
+                string connectionString = builder.Configuration.GetConnectionString(nameof(ProjectVRDbContext));
+                builder.Services.AddDbContext<ProjectVRDbContext>(options =>
+                        options.UseNpgsql(connectionString));
+
+                builder.Services.AddScoped<IUsersService, UsersService>();
+                builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseCors("CorsPolicy");
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("CorsPolicy");
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                logger.Fatal(ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                logger?.Dispose();
+            }
         }
     }
 }
