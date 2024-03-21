66f0a4688a9dd5c80697e109b2c9916b5b792c80
KozhokarIvan <kozhokar.iv@yandex.ru>
2024-03-21 06:13:43 +0300

    fixed closeIcon props

--- src/frontend/project-vr-frontend/src/components/icons/CloseIcon.tsx
+++ src/frontend/project-vr-frontend/src/components/icons/CloseIcon.tsx
@@ -13,7 +13,7 @@ export default function CloseIcon({ className }: CloseIconProps) {
     >
       <path
         d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z"
-        fill-rule="evenodd"
+        fillRule="evenodd"
       />
     </svg>
   );
