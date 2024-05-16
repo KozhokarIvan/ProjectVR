e7dd84932e5fa269322ccba88cc3e7da560b3414
KozhokarIvan <kozhokar.iv@yandex.ru>
2024-05-16 15:39:33 +0300

    feat: updated project to dotnet 8 and added prometheus + otel + grafana

--- src/backend/ProjectVR.WebAPI/Program.cs
+++ src/backend/ProjectVR.WebAPI/Program.cs
@@ -1,9 +1,15 @@
 using System;
-using AspNetCore.Serilog.RequestLoggingMiddleware;
+using System.Diagnostics.Metrics;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing.Matching;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Npgsql;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using ProjectVR.WebAPI.Extensions;
 using Serilog;
 
@@ -14,19 +20,20 @@ public static class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
+        
         var logger = new LoggerConfiguration()
             .ReadFrom.Configuration(builder.Configuration)
             .CreateLogger();
+        Log.Logger = logger;
         try
         {
             builder.Services.AddLogging(configuration =>
             {
                 configuration.ClearProviders();
-                configuration.AddConsole();
                 configuration.AddSerilog(logger);
             });
-
+            builder.Host.UseSerilog(logger);
+            
             // Add services to the container.
             builder.Services.AddCors(options =>
                 options.AddPolicy("CorsPolicy", policyBuilder
@@ -36,12 +43,28 @@ public static class Program
                         .AllowAnyMethod()));
             builder.Services.AddControllers();
 
+            builder.Services
+                .AddOpenTelemetry()
+                .ConfigureResource(b =>
+                {
+                    b.AddService("ProjectVR.WebAPI");
+                })
+                .WithMetrics(opt =>
+                    opt
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddRuntimeInstrumentation()
+                        .AddProcessInstrumentation()
+                        .AddPrometheusExporter()
+                );   
+
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddHttpLogging(options => { });
-
+            
             builder.Services.AddBusinessLogic();
             builder.Services.AddDataAccess(builder.Configuration);
 
@@ -62,6 +85,8 @@ public static class Program
             app.MapControllers();
 
             app.UseSerilogRequestLogging();
+
+            app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
             app.Run();
         }
